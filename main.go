ba6a86bb2be1211e6010c198bff8d239a7addd3a
tienle <tien.le.290803@gmail.com>
2026-05-30 00:30:38 +0700

    create, get 1, list accounts

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	db "github.com/tien29CE/Go_simple_bank.git/db/sqlc"
+	"github.com/tien29CE/Go_simple_bank.git/api"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
